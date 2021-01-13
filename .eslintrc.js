b77d74264d5e31d802117642ffcdceea528edd58
Anita-ihuman <charlesanita403@gmail.com>
2021-01-13 23:09:18 +0100

    fixed eslint bug

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,41 @@
 module.exports = {
-    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
     extends: [
-        'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
-        'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-        +'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-        +'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+        'airbnb-typescript/base',
+        'airbnb/hooks',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:jest/recommended',
+        'prettier',
+        'prettier/react',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
     ],
+    plugins: ['react', '@typescript-eslint', 'jest'],
+    env: {
+        browser: true,
+        es6: true,
+        jest: true,
+    },
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parser: '@typescript-eslint/parser',
     parserOptions: {
-        ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-        sourceType: 'module', // Allows for the use of imports
         ecmaFeatures: {
-            jsx: true, // Allows for the parsing of JSX
+            jsx: true,
         },
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        project: './tsconfig.json',
+        createDefaultProgram: true,
     },
     rules: {
-        // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-        // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-    },
-    settings: {
-        react: {
-            version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
-        },
+        'linebreak-style': 'off',
+        'prettier/prettier': [
+            'error',
+            {
+                endOfLine: 'auto',
+            },
+        ],
     },
 };
