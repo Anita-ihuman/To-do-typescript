7cb6d02ffe0157ab93c372e35393769068767637
Anita-ihuman <charlesanita403@gmail.com>
2021-01-13 23:09:43 +0100

    updated changes made in eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,37 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import { TodoListItem } from './TodoListItem';
-
+import { TodoList } from './TodoList';
+import AddTodoForm from './AddTodoForm';
+import { ToggleTodo, Todo } from './Type';
 
 const initialTodos: Array<Todo> = [
     { text: 'Wlk a dog', complete: true },
     { text: 'feed the dog', complete: true },
 ];
 const App = () => {
-    const [todos, setTodos] = useState(initialTodos)
-    const toggleTodos: ToggleTodo = selectedTodo=> {
-        const newTodos = todos.map(todo => {
+    const [todos, setTodos] = useState(initialTodos);
+
+    const toggleTodos: ToggleTodo = (selectedTodo) => {
+        const newTodos = todos.map((todo) => {
             if (todo === selectedTodo) {
                 return {
                     ...todo,
-                    complete:!todo.complete
-                }
+                    complete: !todo.complete,
+                };
             }
-            return todo
-        })
-        setTodos(newTodos)
-    }
+            return todo;
+        });
+        setTodos(newTodos);
+    };
+
+    const addTodo = (newTodo: string) => {
+        setTodos([...todos, { text: newTodo, complete: false }]);
+    };
+
     return (
         <>
-            <TodoListItem todo={todos[0]} toggleTodos={toggleTodos} />
-            <TodoListItem todo={todos[1]} toggleTodos={toggleTodos} />
+            <TodoList todos={todos} toggleTodos={toggleTodos} />
+            <AddTodoForm />
         </>
     );
 };
