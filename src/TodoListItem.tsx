06dfb3b8bcc9adab6de6ac9f4f1711c3f93e9dbd
Anita-ihuman <charlesanita403@gmail.com>
2021-01-14 15:47:55 +0100

    Fixed errors

--- src/TodoListItem.tsx
+++ src/TodoListItem.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Interface } from 'readline';
+
 import './TodoListItem.css';
 
 interface TodoListItemProps {
@@ -7,14 +7,13 @@ interface TodoListItemProps {
     toggleTodos: ToggleTodo;
 }
 
-export const TodoListItem: React.FC<TodoListItemProps> = ({ todo, toggleTodos  }) => {
-    return <li>
-        <label  className={todo.complete ? 'complete' : undefined}>
-            <input
-                type='checkbox'
-                checked={todo.complete}
-                onChange={() => toggleTodos(todo)}/>
-            {todo.text}
-        </label>
-    </li>;
+export const TodoListItem: React.FC<TodoListItemProps> = ({ todo, toggleTodos }) => {
+    return (
+        <li>
+            <label className={todo.complete ? 'complete' : undefined}>
+                <input type="checkbox" checked={todo.complete} onChange={() => toggleTodos(todo)} />
+                {todo.text}
+            </label>
+        </li>
+    );
 };
